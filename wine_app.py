cf43a46082567dbe67ee1a70781f28e09f8b3b4c
kirbykt <158381377+kirbykt@users.noreply.github.com>
2025-04-05 14:05:17 -0400

    Updated coding for API route in Week 5 assignment

--- wine_app.py
+++ wine_app.py
@@ -6,6 +6,7 @@ Created on Thu Mar 27 12:24:26 2025
 """
 
 from flask import Flask, render_template, request
+from flask import jsonify
 import numpy as np
 import pickle
 
@@ -29,6 +30,14 @@ def predict():
         return render_template('wine_form.html', prediction_text=f'Predicted wine class: {prediction[0]}')
     except Exception as e:
         return render_template('wine_form.html', prediction_text=f'Error: {str(e)}')
+
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    data = request.get_json(force=True)
+    features = [data[str(i)] for i in range(13)]  # expects JSON keys "0" to "12"
+    features = np.array([features], dtype=float)
+    prediction = model.predict(features)
+    return jsonify({'prediction': int(prediction[0])})
     
 if __name__ == '__main__':
     app.run(debug=True)
